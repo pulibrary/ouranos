e20fb1c75c94d80802cb9405a9f2ee6a63aa6734
jrgriffiniii <jrgriffiniii@gmail.com>
2021-06-22 11:03:43 -0400

    Increasing the test coverage to 79.75%

--- spec/lib/ouranos/jobs/environment_lock_spec.rb
+++ spec/lib/ouranos/jobs/environment_lock_spec.rb
@@ -5,22 +5,62 @@ describe Ouranos::Jobs::EnvironmentLock do
   include DeploymentStatusMatchers, EnvironmentLockerMatchers
 
   describe ".perform" do
+    let(:name_with_owner) { "pulibrary/ouranos" }
     let(:lock_params) do
       {
-        name_with_owner: "atmos/heaven",
+        name_with_owner: name_with_owner,
         environment: "production",
-        actor: "atmos",
+        actor: "user",
         deployment_id: "12345"
       }
     end
+    let(:response_body) do
+      {
+        rels: {
+          statuses: {
+            href: 'https://api.github.com/repos/pulibrary/ouranos/status'
+          }
+        }
+      }
+    end
+    let(:request_body) do
+      {
+        target_url: nil,
+        description: "pulibrary/ouranos locked on production by user",
+        state: "success"
+      }
+    end
+    let(:status) { instance_double(Deployment::Status) }
 
-    xit "locks the environment and sends a success status" do
-      job = described_class
+    before do
+      stub_request(:post, "https://api.github.com/repos/pulibrary/ouranos/status").with(
+        headers: {
+          'Accept' => 'application/vnd.github.v3+json',
+          'Authorization' => 'token <unknown>',
+          'Content-Type' => 'application/json'
+        },
+        body: JSON.generate(request_body)
+      ).to_return(status: 200)
 
-      job.perform(lock_params)
+      stub_request(:get, "https://api.github.com/repos/pulibrary/ouranos/deployments/12345").to_return(
+        status: 200,
+        headers: {
+          'Content-Type' => 'application/json'
+        },
+        body: JSON.generate(response_body)
+      )
 
-      expect("atmos/heaven-production").to be_locked
-      expect(Deployment::Status).to have_event("status" => "success")
+      allow(status).to receive(:success!)
+      allow(status).to receive(:description=)
+      allow(Deployment::Status).to receive(:new).and_return(status)
+    end
+
+    it "locks the environment and sends a success status" do
+      described_class.perform(lock_params)
+
+      expect(Deployment::Status).to have_received(:new).with("pulibrary/ouranos", "12345")
+      expect(status).to have_received(:description=).with('pulibrary/ouranos locked on production by user')
+      expect(status).to have_received(:success!)
     end
   end
 end
--- spec/lib/ouranos/jobs/environment_locked_error_spec.rb
+++ spec/lib/ouranos/jobs/environment_locked_error_spec.rb
@@ -13,24 +13,51 @@ describe Ouranos::Jobs::EnvironmentLockedError do
         deployment_id: "12345"
       }
     end
+    let(:api_response) do
+      {
+        rels: []
+      }
+    end
+    let(:api_response_json) do
+      JSON.generate(api_response)
+    end
+    let(:deployment_status) { Deployment::Status.new("atmos/heaven", "12345") }
 
-    xit "triggers an error status with information about the lock" do
-      job = described_class
+    before do
+      allow(Ouranos).to receive(:testing?).and_return(true)
 
-      job.perform(lock_params)
-
-      expect(Deployment::Status).to have_event(
-        "status" => "error",
-        "description" => "atmos/heaven is locked on production by Unknown"
+      stub_request(:get, "https://api.github.com/repos/atmos/heaven/deployments/12345").with(
+        headers: {
+          'Accept' => 'application/vnd.github.v3+json',
+          'Authorization' => 'token <unknown>',
+          'Content-Type' => 'application/json'
+        }
+      ).to_return(
+        status: 200,
+        headers: {
+          'Content-Type' => 'application/json'
+        },
+        body: api_response_json
       )
 
+      allow(Deployment::Status).to receive(:new).and_return(deployment_status)
       EnvironmentLocker.new(lock_params).lock!
 
-      job.perform(lock_params)
+      described_class.perform(lock_params)
+    end
 
-      expect(Deployment::Status).to have_event(
-        "status" => "error",
-        "description" => "atmos/heaven is locked on production by atmos"
+    after do
+      allow(Ouranos).to receive(:testing?).and_call_original
+    end
+
+    it "triggers an error status with information about the lock" do
+      expect(Deployment::Status).to have_received(:new).with("atmos/heaven", "12345")
+      expect(Deployment::Status.deliveries).not_to be_empty
+      expect(Deployment::Status.deliveries.first).to be_a(Hash)
+
+      expect(Deployment::Status.deliveries.last).to include(
+        "description" => "atmos/heaven is locked on production by atmos",
+        "status" => "error"
       )
     end
   end
