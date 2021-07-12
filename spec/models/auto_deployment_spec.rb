f8f7fea071cb57b2a5e2f4808e5dfbaf7b96c90e
jrgriffiniii <jrgriffiniii@gmail.com>
2021-07-12 16:49:32 -0400

    Increasing the coverage to 83.87%

---
+++ spec/models/auto_deployment_spec.rb
@@ -0,0 +1,132 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe AutoDeployment, type: :model do
+  subject(:auto_deployment) { described_class.new(deployment, commit_status) }
+
+  let(:deployment) { instance_double(Deployment) }
+  let(:commit_status) { instance_double(CommitStatus) }
+
+  let(:api_response_repository_disabled) do
+    {
+      id: 369_641_778,
+      node_id: "MDEwOlJlcG9zaXRvcnkzNjk2NDE3Nzg=",
+      name: "heaven",
+      full_name: "atmos/heaven",
+      private: false,
+      owner: {},
+      html_url: "https://github.com/atmos/heaven",
+      description: "A Capistrano-driven deployment service build using https://github.com/atmos/heaven.",
+      fork: false
+    }
+  end
+
+  let(:sha) { "1c66a7a6d0c9055f8de26b9aabeb5d8b9d233146" }
+  let(:author) { "atmos" }
+
+  let(:api_response_statuses) do
+    [
+      {
+        url: "https://api.github.com/repos/atmos/heaven/statuses/1c66a7a6d0c9055f8de26b9aabeb5d8b9d233146",
+        avatar_url: "https://avatars.githubusercontent.com/oa/4808?v=4",
+        id: 13_587_391_075,
+        node_id: "MDEzOlN0YXR1c0NvbnRleHQxMzU4NzM5MTA3NQ==",
+        state: "success",
+        description: "Your tests passed on CircleCI!",
+        target_url: "https://circleci.com/gh/atmos/heaven/168?utm_campaign=vcs-integration-link&utm_medium=referral&utm_source=github-build-link",
+        context: "ci/circleci: lint",
+        created_at: "2021-06-28T15:01:49Z",
+        updated_at: "2021-06-28T15:01:49Z"
+      }
+    ]
+  end
+  let(:api_response) do
+    {
+      state: "success",
+      statuses: api_response_statuses,
+      sha: sha,
+      total_count: 3,
+      repository: {},
+      commit_url: "https://api.github.com/repos/pulibrary/ouranos/commits/1c66a7a6d0c9055f8de26b9aabeb5d8b9d233146",
+      url: "https://api.github.com/repos/pulibrary/ouranos/commits/1c66a7a6d0c9055f8de26b9aabeb5d8b9d233146/status"
+    }
+  end
+  let(:api_response_json) do
+    JSON.generate(api_response)
+  end
+
+  before do
+    allow(commit_status).to receive(:author) { author }
+    allow(commit_status).to receive(:name_with_owner) { "atmos/heaven" }
+    allow(commit_status).to receive(:sha) { sha }
+
+    stub_request(:get, "https://api.github.com/repos/atmos/heaven/commits/1c66a7a6d0c9055f8de26b9aabeb5d8b9d233146/status").with(
+      headers: {
+        'Accept' => 'application/vnd.github.v3+json',
+        'Authorization' => 'token <unknown>',
+        'Content-Type' => 'application/json'
+      }
+    ).to_return(
+      status: 200,
+      headers: {
+        'Content-Type' => 'application/json'
+      },
+      body: api_response_json
+    )
+
+    allow(deployment).to receive(:auto_deploy_payload)
+  end
+
+  describe '#combined_status_green?' do
+    it 'determines if the all deployments were successful' do
+      expect(auto_deployment.combined_status_green?).to be true
+    end
+  end
+
+  describe '#aggregate' do
+    it 'accesses the combined status of all deployments' do
+      expect(auto_deployment.aggregate).not_to be nil
+      expect(auto_deployment.aggregate.to_h).to eq(api_response)
+    end
+  end
+
+  describe '#updated_payload' do
+    before do
+      auto_deployment.updated_payload
+    end
+
+    it 'transmits a payload update using a PATCH request' do
+      expect(deployment).to have_received(:auto_deploy_payload).with(author, sha)
+    end
+  end
+
+  describe '#compare' do
+    it 'derives a comparison between the base deployment and the latest commit' do
+    end
+  end
+
+  describe '#ahead?' do
+    it 'determines if the base deployment is ahead of the latest commit' do
+    end
+  end
+
+  describe '#create_deployment' do
+    it 'transmits a new payload using a POST request' do
+    end
+  end
+
+  describe '#execute' do
+    it 'creates a new deployment' do
+    end
+
+    context 'when the current deployment statuses are not all successes' do
+      it 'does not create a new deployment' do
+      end
+    end
+
+    context 'when the current deployment is behind the latest commit' do
+      it 'does not create a new deployment and logs a message' do
+      end
+    end
+  end
+end
--- spec/models/deployment_spec.rb
+++ spec/models/deployment_spec.rb
@@ -1,6 +1,53 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe Deployment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Deployment, type: :model do
+  describe '.latest_for_name_with_owner' do
+    let(:name_with_owner) do
+      'atmos/heaven'
+    end
+    let(:deployments) do
+      described_class.latest_for_name_with_owner(name_with_owner)
+    end
+    let(:name1) do
+      "atmos1"
+    end
+    let(:name2) do
+      "atmos2"
+    end
+    let(:environment) do
+      "production"
+    end
+    let(:repository) do
+      Repository.create
+    end
+    let(:deployment1) do
+      described_class.create(name_with_owner: name_with_owner, name: name1, environment: environment, repository: repository)
+    end
+    let(:deployment2) do
+      described_class.create(name_with_owner: name_with_owner, name: name2, environment: environment, repository: repository)
+    end
+
+    before do
+      deployment1
+      deployment2
+    end
+
+    it 'finds all of the Deployment models' do
+      expect(deployments).not_to be_empty
+
+      expect(deployments.first).to be_a(described_class)
+      expect(deployments).to include(deployment1, deployment2)
+    end
+  end
+
+  describe '#payload' do
+    it '' do
+    end
+  end
+
+  describe '#auto_deploy_payload' do
+    it '' do
+    end
+  end
 end
