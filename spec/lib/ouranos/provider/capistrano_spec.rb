1a109d3af1b1a52a8674c4bd1bfadac7f74739fb
jrgriffiniii <jrgriffiniii@gmail.com>
2021-07-13 09:48:36 -0400

    Increasing the coverage to 84.44%

--- spec/lib/ouranos/provider/capistrano_spec.rb
+++ spec/lib/ouranos/provider/capistrano_spec.rb
@@ -9,4 +9,39 @@ describe Ouranos::Provider::Capistrano do
   it "finds deployment task" do
     expect(deployment.task).to eql "deploy:migrations"
   end
+
+  describe '#cap_path' do
+    it 'accesses the Capistrano executable path' do
+      expect(deployment.cap_path).to eq('bin/cap')
+    end
+  end
+
+  describe '#execute' do
+    let(:last_child) do
+      instance_double(POSIX::Spawn::Child)
+    end
+
+    before do
+      allow(last_child).to receive(:out).and_return("".dup)
+      allow(last_child).to receive(:err).and_return("".dup)
+      allow(last_child).to receive(:success?).and_return(true)
+
+      allow(POSIX::Spawn::Child).to receive(:new).and_return(last_child)
+
+      deployment.execute
+    end
+
+    it 'clones the repository into the checkout directory' do
+      expect(deployment.working_directory).not_to be_empty
+      expect(File.exist?(deployment.working_directory)).to be true
+
+      expect(POSIX::Spawn::Child).to have_received(:new).with(
+        {
+          "HOME" => deployment.working_directory
+        },
+        "/usr/bin/true",
+        {}
+      )
+    end
+  end
 end
